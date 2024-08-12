331eadc928088865f0983399ba4c4a70079192f7
Katharina Lehmann <131286549+Walfish82@users.noreply.github.com>
2024-08-12 14:35:42 +0200

    Update and rename SQL Portfolio Project_covid.sql to SQL EDA Portfolio Project_covid.sql

--- SQL EDA Portfolio Project_covid.sql
+++ SQL EDA Portfolio Project_covid.sql
@@ -1,3 +1,10 @@
+/*
+Covid 19 Data Exploration 
+
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
+*/
+
 use PortfolioProject;
 
 SELECT * FROM covid_deaths
